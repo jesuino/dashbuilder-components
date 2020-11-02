d0d7f80f74b23802c3ba5c170abaa4c85d562d0e
manaswinidas <dasmanaswini10@gmail.com>
2020-11-02 23:14:49 +0530

    [AF-2701] add treemap

---
+++ treemap/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import  {Treemaps} from './Treemaps';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Treemaps />
+    </div>
+  );
+}
+
+export default App;
