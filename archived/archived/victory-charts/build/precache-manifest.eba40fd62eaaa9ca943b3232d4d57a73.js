a1c9750eb9cde5592e53afb139ab15141830d0a3
William Siqueira <william.fatecsjc@gmail.com>
2020-12-10 17:39:06 -0300

    Archiving existing components

---
+++ archived/archived/victory-charts/build/precache-manifest.eba40fd62eaaa9ca943b3232d4d57a73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "464b9da008e981a06bbb459f942d59ff",
+    "url": "./index.html"
+  },
+  {
+    "revision": "451a22582b8fdbb2b4fd",
+    "url": "./static/css/2.6618b920.chunk.css"
+  },
+  {
+    "revision": "d41865826a7f3bcdadc2",
+    "url": "./static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "451a22582b8fdbb2b4fd",
+    "url": "./static/js/2.3ea54d97.chunk.js"
+  },
+  {
+    "revision": "a172610bddb18aa655329c9295c373c0",
+    "url": "./static/js/2.3ea54d97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d41865826a7f3bcdadc2",
+    "url": "./static/js/main.db925532.chunk.js"
+  },
+  {
+    "revision": "217b8c9e76bfea79dc9f",
+    "url": "./static/js/runtime-main.27fa7950.js"
+  }
+]);
