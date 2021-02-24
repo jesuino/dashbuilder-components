694ef8c8dabac508c312f4932745b34df26c41d7
manaswinidas <dasmanaswini10@gmail.com>
2021-02-24 22:04:56 +0530

    remove linting errors and replace with transposed

--- packages/time-series-chart/src/Chart.tsx
+++ packages/time-series-chart/src/Chart.tsx
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import * as React from "react";
 import { LineChart, ChartProps } from "./LineChart";
 import { options, series } from "./SampleData";
@@ -9,7 +25,6 @@ import { Options, SingleSeries } from "./Data";
 const NOT_ENOUGH_COLUMNS_MSG =
   "Time series component expects at least 2 columns: category(LABEL or TEXT or NUMBER or DATE) and one or more series(NUMBER).";
 const FIRST_COLUMN_INVALID_MSG = "Wrong type for first column, it should be either LABEL, TEXT, NUMBER or DATE.";
-const SECOND_COLUMN_INVALID_MSG = "Wrong type for second column, it should be NUMBER.";
 const NOT_ENOUGH_COLUMNS_MSG_1 =
   "Time series component expects 3 columns: category(LABEL or TEXT or NUMBER or DATE), series(TEXT) and values(NUMBER).";
 const FIRST_COLUMN_INVALID_MSG_1 = "Wrong type for first column, it should be either LABEL, TEXT, NUMBER or DATE.";
@@ -17,7 +32,7 @@ const SECOND_COLUMN_INVALID_MSG_1 = "Wrong type for second column, it should be 
 const THIRD_COLUMN_INVALID_MSG_1 = "Wrong type for third column, it should be NUMBER.";
 
 enum Params {
-  FLAG = "flag",
+  TRANSPOSED = "transposed",
   CHARTNAME = "chartName"
 }
 
@@ -32,55 +47,48 @@ enum AppStateType {
 interface AppState {
   state: AppStateType;
   processesoptions: Options;
-  processesseries: Array<SingleSeries>;
+  processesseries: SingleSeries[];
   configurationIssue: string;
   message?: string;
 }
 
 const validateParams = (params: Map<string, string | number>): string | undefined => {
-  if (!params.get(Params.FLAG)) {
-    return "Flag is required.";
+  if (!params.get(Params.TRANSPOSED)) {
+    return "Transposed is required.";
   }
   if (!params.get(Params.CHARTNAME)) {
     return "Chart name is required.";
   }
 };
 
-const validateDataSet = (ds: DataSet, flag: number): string | undefined => {
-  if (flag === 0) {
-    if (ds.columns.length < 2) {
-      return NOT_ENOUGH_COLUMNS_MSG;
-    }
-    if (
-      ds.columns[0].type !== ColumnType.LABEL &&
-      ds.columns[0].type !== ColumnType.TEXT &&
-      ds.columns[0].type !== ColumnType.NUMBER &&
-      ds.columns[0].type !== ColumnType.DATE
-    ) {
-      return FIRST_COLUMN_INVALID_MSG;
-    }
-    if (ds.columns[1].type !== ColumnType.NUMBER) {
-      return SECOND_COLUMN_INVALID_MSG;
+const validateDataSet = (ds: DataSet, transposed: boolean) => {
+  if (transposed) {
+    validateTransposedDataset(ds);
+  } else {
+    validateNonTransposedDataset(ds);
+  }
+};
+
+const validateNonTransposedDataset = (ds: DataSet): string | undefined => {
+  if (ds.columns.length < 2) {
+    return NOT_ENOUGH_COLUMNS_MSG;
+  }
+  for (let i = 1; i < ds.columns.length; i++) {
+    if (ds.columns[i].type !== ColumnType.NUMBER) {
+      return "Wrong type for column" + (i + 1) + ", it should be NUMBER";
     }
   }
-  if (flag === 1) {
-    if (ds.columns.length < 3) {
-      return NOT_ENOUGH_COLUMNS_MSG_1;
-    }
-    if (
-      ds.columns[0].type !== ColumnType.LABEL &&
-      ds.columns[0].type !== ColumnType.TEXT &&
-      ds.columns[0].type !== ColumnType.NUMBER &&
-      ds.columns[0].type !== ColumnType.DATE
-    ) {
-      return FIRST_COLUMN_INVALID_MSG_1;
-    }
-    if (ds.columns[1].type !== ColumnType.TEXT && ds.columns[1].type !== ColumnType.LABEL) {
-      return SECOND_COLUMN_INVALID_MSG_1;
-    }
-    if (ds.columns[2].type !== ColumnType.NUMBER) {
-      return THIRD_COLUMN_INVALID_MSG_1;
-    }
+};
+
+const validateTransposedDataset = (ds: DataSet): string | undefined => {
+  if (ds.columns.length < 3) {
+    return NOT_ENOUGH_COLUMNS_MSG_1;
+  }
+  if (ds.columns[1].type !== ColumnType.TEXT && ds.columns[1].type !== ColumnType.LABEL) {
+    return SECOND_COLUMN_INVALID_MSG_1;
+  }
+  if (ds.columns[2].type !== ColumnType.NUMBER) {
+    return THIRD_COLUMN_INVALID_MSG_1;
   }
 };
 
@@ -88,51 +96,63 @@ interface Props {
   controller: ComponentController;
 }
 
-function getseries(dataset: DataSet, flag: number): Array<SingleSeries> {
-  var arrayseries: Array<SingleSeries> = [];
-  var newseries: SingleSeries = { name: "", data: [] };
-  if (flag === 0) {
-    for (var i = 1; i < dataset.columns.length; i++) {
-      newseries = { name: dataset.columns[i].name, data: dataset.data.map((d: Array<string | number>) => d[i]) };
-      arrayseries.push(newseries);
-    }
-  }
-  if (flag === 1) {
-    var names: Array<string> = [];
-    var datum: Array<string | number> = [];
-    var newnames: Array<string> = [];
-    names = dataset.data.map((d: Array<string>) => d[1]);
-    datum = dataset.data.map((d: Array<string | number>) => d[2]);
-    for (var i = 0; i < names.length; i++) {
-      if (newnames.indexOf(names[i]) == -1) {
-        newnames.push(names[i]);
-      }
-    }
-    for (var i = 0; i < newnames.length; i++) {
-      var newdata: Array<number | string> = [];
-      for (var j = 0; j < names.length; j++) {
-        if (newnames[i] == names[j]) {
-          newdata.push(datum[j]);
-        }
-      }
-      newseries = { name: newnames[i], data: newdata };
-      arrayseries.push(newseries);
-    }
+function getSeries(dataset: DataSet, transposed: boolean): SingleSeries[] {
+  let arrayseries: SingleSeries[] = [];
+  if (transposed) {
+    arrayseries = getSeriesforTransposedDataset(dataset);
+  } else {
+    arrayseries = getSeriesforNonTransposedDataset(dataset);
   }
   return arrayseries;
 }
 
-function getoptions(dataset: DataSet, flag: number, chartName: string): Options {
+function getSeriesforNonTransposedDataset(dataset: DataSet): SingleSeries[] {
+  const arrayseries: SingleSeries[] = [];
+  let newseries: SingleSeries = { name: "", data: [] };
+  for (let i = 1; i < dataset.columns.length; i++) {
+    newseries = { name: dataset.columns[i].name, data: dataset.data.map((d: Array<string | number>) => d[i]) };
+    arrayseries.push(newseries);
+  }
+  return arrayseries;
+}
+
+function getSeriesforTransposedDataset(dataset: DataSet): SingleSeries[] {
+  const arrayseries: SingleSeries[] = [];
+  let newseries: SingleSeries = { name: "", data: [] };
+  let names: string[] = [];
+  let datum: Array<string | number> = [];
+  const newnames: string[] = [];
+  names = dataset.data.map((d: string[]) => d[1]);
+  datum = dataset.data.map((d: Array<string | number>) => d[2]);
+  for (const i of names) {
+    if (newnames.indexOf(i) === -1) {
+      newnames.push(i);
+    }
+  }
+  for (const i of newnames) {
+    const newdata: Array<number | string> = [];
+    for (let j = 0; j < names.length; j++) {
+      if (i === names[j]) {
+        newdata.push(datum[j]);
+      }
+    }
+    newseries = { name: i, data: newdata };
+    arrayseries.push(newseries);
+  }
+  return arrayseries;
+}
+
+function getOptions(dataset: DataSet, transposed: boolean, chartName: string): Options {
   function removeduplicates(arr: Array<string | number>): Array<string | number> {
-    let outputArray: Array<string | number> = Array.from(new Set(arr));
+    const outputArray: Array<string | number> = Array.from(new Set(arr));
     return outputArray;
   }
-  var newoptions: Options = { chart: { id: "" }, xaxis: { categories: [] } };
+  let newoptions: Options = { chart: { id: "" }, xaxis: { categories: [] } };
   newoptions = {
     chart: { id: chartName },
     xaxis: { categories: dataset.data.map((d: Array<string | number>) => d[0]) }
   };
-  if (flag === 1) {
+  if (transposed) {
     newoptions.xaxis.categories = removeduplicates(newoptions.xaxis.categories);
   }
   return newoptions;
@@ -140,8 +160,8 @@ function getoptions(dataset: DataSet, flag: number, chartName: string): Options 
 
 export function Chart(props: Props) {
   const [chartProps, setChartProps] = useState<ChartProps>({
-    options: options,
-    series: series
+    options,
+    series
   });
   const [appState, setAppState] = useState<AppState>({
     state: AppStateType.INIT,
@@ -150,7 +170,7 @@ export function Chart(props: Props) {
     configurationIssue: ""
   });
   const onDataset = useCallback((ds: DataSet, params: Map<string, any>) => {
-    const validationMessage = validateParams(params) || validateDataSet(ds, params.get(Params.FLAG));
+    const validationMessage = validateParams(params) || validateDataSet(ds, params.get(Params.TRANSPOSED));
     if (validationMessage) {
       setAppState(previousAppState => ({
         ...previousAppState,
@@ -161,8 +181,8 @@ export function Chart(props: Props) {
     } else {
       setAppState(previousAppState => ({
         ...previousAppState,
-        processesoptions: getoptions(ds, params.get(Params.FLAG), params.get(Params.CHARTNAME)),
-        processesseries: getseries(ds, params.get(Params.FLAG)),
+        processesoptions: getOptions(ds, params.get(Params.TRANSPOSED), params.get(Params.CHARTNAME)),
+        processesseries: getSeries(ds, params.get(Params.TRANSPOSED)),
         state: AppStateType.FINISHED,
         configurationIssue: ""
       }));
